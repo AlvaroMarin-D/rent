34c70d879cac5276f8dd3d21803229c1f601596d
AlvaroMarin-D <alvaromarin.d@gmail.com>
2022-09-28 19:14:15 -0400

    property pages update

--- pages/search.js
+++ pages/search.js
@@ -6,6 +6,7 @@ import {BsFilter} from 'react-icons/bs'
 import SearchFilters from "../components/SearchFilters";
 import Property from "../components/Property";
 import noresult from "../assets/images/noresult.svg"
+import {baseUrl,fetchApi} from '../utils/fetchApi'
 
 const Search = ({Properties}) => {
     const [searchFilters,setSearchFilters] = useState(false);
@@ -33,30 +34,41 @@ const Search = ({Properties}) => {
                 Properties {router.query.purpose}
             </Text>
             <Flex flexWrap="wrap">
-                {/* {Properties.map((property) => <Property property={property} key={property.id}/>)} */}
+                {Properties.map((property) => <Property property={property} key={property.id}/>)}
             </Flex>
-            {/* {Properties.length ===0 &&(
+            {Properties.length ===0 &&(
                 <Flex justifyContent="center" alignItems="center" flexDirection="column" marginTop="5" marginBottom="5">
                     <Image alt="no result" src={noresult}/>
                     <Text fontSize="2xl" marginTop="3">
                         No Results Found
                     </Text>
                 </Flex>
-            )} */}
+            )}
         </Box>
     )
 }
 
-export default Search;
 
-// export async function getStaticProps({query}) {
-//     const purpose = query.purpose
-  
-//     return {
-//       props:{
-//         propertiesForSale: propertyForSale?.hits,
-//         propertiesForRent: propertyForRent?.hits,
-//       }
-//     }
-//   }
-  
+
+export async function getServerSideProps({query}) {
+    const purpose = query.purpose || 'for-rent';
+    const rentFrequency = query.rentFrequency || 'yearly';
+    const minPrice = query.minPrice || '0';
+    const maxPrice = query.maxPrice || '1000000';
+    const roomsMin = query.roomsMin || '0';
+    const bathsMin = query.bathsMin || '0';
+    const sort = query.sort || 'price-desc';
+    const areaMax = query.areaMax || '35000';
+    const locationExternalIDs = query.locationExternalIDs || '5002';
+    const categoryExternalID = query.categoryExternalID || '4';
+
+    const data = await fetchApi(`${baseUrl}/properties/list?locationExternalIDs=${locationExternalIDs}&purpose=${purpose}&categoryExternalID=${categoryExternalID}&bathsMin=${bathsMin}&rentFrequency=${rentFrequency}&priceMin=${minPrice}&priceMax=${maxPrice}&roomsMin=${roomsMin}&sort=${sort}&areaMax=${areaMax}`);
+
+  return {
+    props: {
+      Properties: data?.hits,
+    },
+  };
+}
+
+export default Search;
